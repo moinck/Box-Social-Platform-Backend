20d8186bc9d232fd8b0ce0bb5f2b7484111f0e25
Martik IIH <martik.iihglobal@gmail.com>
2025-05-14 18:07:27 +0530

    notification

--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -43,7 +43,60 @@ $navbarDetached = ($navbarDetached ?? '');
      
 
       <ul class="navbar-nav flex-row align-items-center ms-auto">
-
+      <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-4 me-xl-1">
+            <a class="nav-link btn btn-text-secondary rounded-pill btn-icon dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+              <i class="ri-notification-2-line ri-22px"></i>
+              <span class="position-absolute top-0 start-50 translate-middle-y badge badge-dot bg-danger mt-2 border"></span>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end py-0">
+              <li class="dropdown-menu-header border-bottom py-50">
+                <div class="dropdown-header d-flex align-items-center py-2">
+                  <h6 class="mb-0 me-auto">Notification</h6>
+                  <div class="d-flex align-items-center">
+                    <span class="badge rounded-pill bg-label-primary fs-xsmall me-2">8 New</span>
+                    <a href="javascript:void(0)" class="btn btn-text-secondary rounded-pill btn-icon dropdown-notifications-all" data-bs-toggle="tooltip" data-bs-placement="top" title="Mark all as read"><i class="ri-mail-open-line text-heading ri-20px"></i></a>
+                  </div>
+                </div>
+              </li>
+              <li class="dropdown-notifications-list scrollable-container">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item list-group-item-action dropdown-notifications-item">
+                    <div class="d-flex">
+                      <div class="flex-shrink-0 me-3">
+                        <div class="avatar">
+                          <img src="{{asset('assets/img/avatars/1.png')}}" alt class="rounded-circle">
+                        </div>
+                      </div>
+                      <div class="flex-grow-1">
+                        <h6 class="small mb-1">Congratulation Lettie 🎉</h6>
+                        <small class="mb-1 d-block text-body">Won the monthly best seller gold badge</small>
+                        <small class="text-muted">1h ago</small>
+                      </div>
+                      <div class="flex-shrink-0 dropdown-notifications-actions">
+                        <a href="javascript:void(0)" class="dropdown-notifications-read"><span class="badge badge-dot"></span></a>
+                        <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="ri-close-line ri-20px"></span></a>
+                      </div>
+                    </div>
+                  </li>
+                  
+                 
+                 
+                 
+                 
+                  
+                  
+                  
+                </ul>
+              </li>
+              <li class="border-top">
+                <div class="d-grid p-4">
+                  <a class="btn btn-primary btn-sm d-flex" href="javascript:void(0);">
+                    <small class="align-middle">View all notifications</small>
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </li>
         <!-- User -->
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
           <a class="#04202E" href="javascript:void(0);" data-bs-toggle="dropdown">
