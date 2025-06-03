1840a14367449aa3c7ee605a4097ebfd385d3b09
pratik dev <pratikdev.iihglobal@gmail.com>
2025-06-03 17:55:27 +0530

    ADmin -> added design-style changes to brand-kit table & API

---
+++ database/migrations/2025_06_03_112506_add_design_style_id_to_brand_kits_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('brand_kits', function (Blueprint $table) {
+            $table->unsignedBigInteger('design_style_id')->nullable()->after('user_id');
+            $table->foreign('design_style_id')->references('id')->on('design_styles')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('brand_kits', function (Blueprint $table) {
+            $table->dropForeign(['design_style_id']);
+            $table->dropColumn('design_style_id');
+        });
+    }
+};
