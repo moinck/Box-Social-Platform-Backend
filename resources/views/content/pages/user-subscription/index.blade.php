9e5fc128a90f65877230effbbb1905654d4f0368
pratik dev <pratikdev.iihglobal@gmail.com>
2025-07-22 14:19:28 +0530

    Admin => new subscription management module added

---
+++ resources/views/content/pages/user-subscription/index.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts/layoutMaster')
+
+@section('title', 'Subscription Management')
+
+<!-- Vendor Styles -->
+@section('vendor-style')
+    @vite([
+        'resources/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-select-bs5/select.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.scss',
+        'resources/assets/vendor/libs/datatables-fixedcolumns-bs5/fixedcolumns.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-fixedheader-bs5/fixedheader.bootstrap5.scss',
+        'resources/assets/vendor/libs/@form-validation/form-validation.scss',
+        'resources/assets/vendor/libs/sweetalert2/sweetalert2.scss'
+    ])
+@endsection
+
+<!-- Vendor Scripts -->
+@section('vendor-script')
+    @vite([
+        'resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js',
+        'resources/assets/vendor/libs/@form-validation/popular.js',
+        'resources/assets/vendor/libs/@form-validation/bootstrap5.js',
+        'resources/assets/vendor/libs/@form-validation/auto-focus.js',
+        'resources/assets/vendor/libs/sweetalert2/sweetalert2.js'
+    ])
+@endsection
+
+
+
+@section('content')
+
+    <!-- Main Table -->
+    <div class="card">
+        <div class="card-header d-flex flex-column flex-md-row border-bottom subscription-management-header">
+            <div class="head-label">
+                <h5 class="card-title mb-0">Subscription Management</h5>
+            </div>
+        </div>
+        <div class="card-datatable table-responsive">
+            <table class="dt-fixedheader table table-bordered" id="subscription-management-data-table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>User</th>
+                        <th>Subscription Plan</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Status</th>
+                        <th>Created Date</th>
+                        <th>Updated Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <!--/ Main Table -->
+
+    <!--/ Select -->
+@endsection
+
+<!-- Page Scripts -->
+@section('page-script')
+    {{-- @vite(['resources/assets/js/tables-datatables-extensions.js']) --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            UserSubscriptionDataTable();
+
+            // user subscription data table function
+            function UserSubscriptionDataTable() {
+                var UserSubscriptionTable = $('#subscription-management-data-table').DataTable({
+                    bLengthChange: false,
+                    searchable: true,
+                    serverSide: true,
+                    orderable: true,
+                    searching: true,
+                    destroy: true,
+                    info: true,
+                    paging: true,
+                    pageLength: 10,
+                    ajax: {
+                        url: "{{ route('subscription-management.data-table') }}",
+                        beforeSend: function () {
+                            showBSPLoader();
+                        },
+                        complete: function () {
+                            hideBSPLoader();
+                        }
+                    },
+                    columns: [
+                        { data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                        { data: 'user', name: 'user'},
+                        { data: 'plan', name: 'plan'},
+                        { data: 'start_date', name: 'start_date'},
+                        { data: 'end_date', name: 'end_date'},
+                        { data: 'status', name: 'status'},
+                        { data: 'created_date', name: 'created_date'},
+                        { data: 'updated_date', name: 'updated_date'},
+                        { data: 'action', name: 'action', orderable: false, searchable: false},
+                    ],
+                    language: {
+                        paginate: {
+                            next: '<i class="ri-arrow-right-s-line"></i>',
+                            previous: '<i class="ri-arrow-left-s-line"></i>'
+                        }
+                    },
+                    drawCallback: function(settings) {
+                        $('[data-bs-toggle="tooltip"]').tooltip();
+                    }
+                });
+            }
+            // -------------------------------------------
+
+            // delete brand configuration
+            $(document).on('click', '.delete-user-subscription-btn-1', function() {
+                var userSubscriptionId = $(this).data('user-subscription-id');
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, delete it!',
+                    customClass: {
+                        confirmButton: 'btn btn-primary me-3',
+                        cancelButton: 'btn btn-outline-secondary'
+                    },
+                    buttonsStyling: false
+                }).then(function(result) {
+                    if (result.value) {
+                        $.ajax({
+                            url: "{{ route('brand-configuration.delete') }}",
+                            type: "POST",
+                            data: {
+                                _token: '{{ csrf_token() }}',
+                                brand_kit_id: brandKitId
+                            },
+                            beforeSend: function () {
+                                showBSPLoader();
+                            },
+                            complete: function () {
+                                hideBSPLoader();
+                            },
+                            success: function(response) {
+                                if (response.success == true) {
+                                    showSweetAlert('success', 'Deleted!', 'Brand Configuration has been deleted.');
+                                    BrandConfigurationDataTable();
+                                } else {
+                                    showSweetAlert('error', 'Error!', 'Something went wrong.');
+                                }
+                            },
+                            error: function(xhr, status, error) {
+                                hideBSPLoader();
+                                console.log(xhr.responseText);
+                                showSweetAlert('error', 'Error!', 'Something went wrong.');
+                            }
+                        });
+                    }
+                });
+            });
+            // ----------------------------------------------------------
+        });
+    </script>
+@endsection
