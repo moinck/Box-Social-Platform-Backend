104769c039d2841db3de7705bc385f825698b218
Martik IIH <martik.iihglobal@gmail.com>
2025-08-07 17:13:44 +0530

    cron

--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,30 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This configuration determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // Allow all HTTP methods (GET, POST, etc.)
 
-    'allowed_origins' => [env('FRONTEND_URL')], // or your React app URL
+    'allowed_origins' => ['http://178.128.45.173:9163','http://localhost:3000'], // React app origin
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // Allow all headers (Authorization, Content-Type, etc.)
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], // Headers exposed to the browser (if needed)
 
-    'max_age' => 0,
+    'max_age' => 0, // How long the results of a preflight request can be cached
 
-    'supports_credentials' => true, // set to true only if using cookies
+    'supports_credentials' => true, // Set to true if using cookies/sessions
 
 ];
