5706ef003f73f263a49c1041ed2d7d573646ca83
ronakiih <ronak.iihglobal@gmail.com>
2025-08-11 12:46:16 +0000

    Commite live changes

--- database/migrations/2025_05_23_110153_add_profile_image_to_users_table.php
+++ database/migrations/2025_05_23_110153_add_profile_image_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_image')->nullable()->after('website');
+            $table->string('profile_image')->nullable();
         });
     }
 
