8b5c4055efc627f5f5d5cb2a9ac87686180be481
pratik dev <pratikdev.iihglobal@gmail.com>
2025-08-22 17:52:10 +0530

    Admin => added common function to delete user's data

--- app/Http/Controllers/Api/UserManagementApiController.php
+++ app/Http/Controllers/Api/UserManagementApiController.php
@@ -39,87 +39,11 @@ class UserManagementApiController extends Controller
             return $this->error('User not found', 404);
         }
 
-        try {
-            DB::beginTransaction();
-
-            // add fca number
-            FcaNumbers::updateOrCreate([
-                'fca_number' => $user->fca_number,
-            ], [
-                'fca_name' => $user->company_name,
-            ]);
-    
-            // stock image delete
-            // $user->imageStockManagement()->delete();
-            $allDeleteImageData = ImageStockManagement::where('user_id', $userId)->get();
-    
-            foreach ($allDeleteImageData as $value) {
-                Helpers::deleteImage($value->image_url);
-                $value->delete();
-            }
-    
-            // brand kit delete
-            // $user->brandKit()->delete();
-            $userBrandKit = BrandKit::where('user_id', $userId)->first();
-            if (!empty($userBrandKit)) {
-                Helpers::deleteImage($userBrandKit->logo);
-                $userBrandKit->delete();
-            }
-    
-            // subscription (all) delete
-            // $user->subscription()->delete();
-            $userSubscription = UserSubscription::where('user_id', $userId)->get();
-            if (!empty($userSubscription)) {
-                foreach ($userSubscription as $value) {
-                    if ($value->plan_id != 1) {
-                        // first cancel subscription from stripe
-                        $this->stripe->subscriptions->cancel($value->stripe_subscription_id, [
-                            'cancellation_details' => [
-                                'comment' => 'user deleted their account',
-                                'reason' => 'account_deleted',
-                            ],
-                        ]);
-    
-                        $value->delete();
-                    } else {
-                        $value->delete();
-                    }
-                }
-            }
-    
-            // user downloads
-            $userDownloads = UserDownloads::where('user_id', $userId)->get();
-            if (!empty($userDownloads)) {
-                foreach ($userDownloads as $value) {
-                    $value->delete();
-                }
-            }
-    
-            // user template delete
-            $userTemplate = UserTemplates::where('user_id', $userId)->get();
-            if (!empty($userTemplate)) {
-                foreach ($userTemplate as $value) {
-                    Helpers::deleteImage($value->template_image);
-                    $value->delete();
-                }
-            }
-    
-            // delete usertokens
-            $userTokens = UserTokens::where('user_id', $userId)->get();
-            if (!empty($userTokens)) {
-                foreach ($userTokens as $value) {
-                    $value->delete();
-                }
-            }
-    
-            $user->delete();
-            DB::commit();
-            return $this->success([],'Your account deleted successfully.');
-        } catch (Exception $e) {
-            DB::rollBack();
-            dd($e);
-            Helpers::sendErrorMailToDeveloper($e,'User account delete API.');
-            return $this->error('something went wrong', 500);
+        $deleteUser = Helpers::deleteUserData($userId);
+        if ($deleteUser === true) {
+            return $this->success([], 'Your Account deleted successfully');
+        } else {
+            return $this->error('Something went wrong', 500);
         }
     }
 }
