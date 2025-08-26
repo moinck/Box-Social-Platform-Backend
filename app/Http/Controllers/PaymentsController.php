3bf1afe0295da2f08383c160739ab130f44af056
pratik dev <pratikdev.iihglobal@gmail.com>
2025-08-26 15:11:46 +0530

    Admin => new payment history module added

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Helpers;
+use Illuminate\Http\Request;
+use App\Models\Payments;
+use App\Models\UserSubscription;
+use Yajra\DataTables\Facades\DataTables;
+
+class PaymentsController extends Controller
+{
+    public function index()
+    {
+        return view('content.pages.payments.index');
+    }
+
+    public function dataTable(Request $request)
+    {
+        $payments = Payments::with('user:id,first_name,last_name,email')
+            ->latest()
+            ->get();
+
+        return DataTables::of($payments)
+            ->addIndexColumn()
+            ->addColumn('user', function ($payment) {
+                $name = $payment->user->first_name . ' ' . $payment->user->last_name;
+                return $name;
+            })
+            ->addColumn('payment_id', function ($payment) {
+                if ($payment->stripe_payment_intent_id) {
+                    $subscription = UserSubscription::select('id','stripe_subscription_id')->where('stripe_payment_method_id', $payment->stripe_payment_intent_id)->first();
+                    return $subscription ? $subscription->stripe_subscription_id : 'N/A';
+                }
+                return 'N/A';
+            })
+            ->addColumn('plan', function ($payment) {
+                return $payment->plan_name;
+            })
+            ->addColumn('amount', function ($payment) {
+                return $payment->amount . ' ' . strtoupper($payment->currency);
+            })
+            ->addColumn('payment_method', function ($payment) {
+                return $payment->payment_method ? strtoupper($payment->payment_method) : 'N/A';
+            })
+            ->addColumn('created_date', function ($payment) {
+                return Helpers::dateFormate($payment->created_at);
+            })
+            ->rawColumns(['user', 'plan', 'amount', 'payment_method', 'created_date'])
+            ->make(true);
+    }
+}
---
+++ resources/views/content/pages/payments/index.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts/layoutMaster')
+
+@section('title', 'Payment history')
+
+<!-- Vendor Styles -->
+@section('vendor-style')
+    @vite([
+        'resources/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-select-bs5/select.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.scss',
+        'resources/assets/vendor/libs/datatables-fixedcolumns-bs5/fixedcolumns.bootstrap5.scss',
+        'resources/assets/vendor/libs/datatables-fixedheader-bs5/fixedheader.bootstrap5.scss',
+        'resources/assets/vendor/libs/@form-validation/form-validation.scss',
+        'resources/assets/vendor/libs/sweetalert2/sweetalert2.scss'
+    ])
+@endsection
+
+<!-- Vendor Scripts -->
+@section('vendor-script')
+    @vite([
+        'resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js',
+        'resources/assets/vendor/libs/@form-validation/popular.js',
+        'resources/assets/vendor/libs/@form-validation/bootstrap5.js',
+        'resources/assets/vendor/libs/@form-validation/auto-focus.js',
+        'resources/assets/vendor/libs/sweetalert2/sweetalert2.js'
+    ])
+@endsection
+
+
+
+@section('content')
+
+    <!-- Main Table -->
+    <div class="card">
+        <div class="card-header d-flex flex-column flex-md-row border-bottom subscription-management-header" style="justify-content: space-between;">
+            <div class="head-label">
+                <h5 class="card-title mb-0">Payment history</h5>
+            </div>
+        </div>
+        <div class="card-datatable table-responsive">
+            <table class="dt-fixedheader table table-bordered" id="payment-management-data-table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>User</th>
+                        <th>Payment Method ID</th>
+                        <th>Subscription Plan</th>
+                        <th>Amount</th>
+                        <th>Payment Method</th>
+                        <th>Payment Date</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <!--/ Main Table -->
+
+    {{-- data export modal --}}
+    <div class="modal fade" id="data-export-modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modalCenterTitle">Export Data</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col mb-6 mt-2 text-center">
+                            <h4 class="mb-4">Select Export Format</h4>
+                            <div class="d-flex justify-content-center gap-4">
+                                <button type="button" id="csv-export-btn" title="Export users in CSV format" class="btn btn-primary">CSV</button>
+                                <button type="button" id="excel-export-btn" title="Export users in Excel format" class="btn btn-primary">Excel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--/ Select -->
+@endsection
+
+<!-- Page Scripts -->
+@section('page-script')
+    {{-- @vite(['resources/assets/js/tables-datatables-extensions.js']) --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            PaymentDataTable();
+
+            // user subscription data table function
+            function PaymentDataTable() {
+                var PaymentTable = $('#payment-management-data-table').DataTable({
+                    bLengthChange: false,
+                    searchable: true,
+                    serverSide: true,
+                    orderable: true,
+                    searching: true,
+                    destroy: true,
+                    info: true,
+                    paging: true,
+                    pageLength: 10,
+                    ajax: {
+                        url: "{{ route('payment-history.data-table') }}",
+                        beforeSend: function () {
+                            showBSPLoader();
+                        },
+                        complete: function () {
+                            hideBSPLoader();
+                        }
+                    },
+                    columns: [
+                        { data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                        { data: 'user', name: 'user'},
+                        { data: 'payment_id', name: 'payment_id'},
+                        { data: 'plan_name', name: 'plan_name'},
+                        { data: 'amount', name: 'amount'},
+                        { data: 'payment_method', name: 'payment_method'},
+                        { data: 'created_date', name: 'created_date'},
+                    ],
+                    language: {
+                        paginate: {
+                            next: '<i class="ri-arrow-right-s-line"></i>',
+                            previous: '<i class="ri-arrow-left-s-line"></i>'
+                        }
+                    },
+                    drawCallback: function(settings) {
+                        $('[data-bs-toggle="tooltip"]').tooltip();
+                    }
+                });
+            }
+            // -------------------------------------------
+        });
+    </script>
+@endsection
