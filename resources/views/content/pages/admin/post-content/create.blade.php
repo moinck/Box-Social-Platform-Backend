ed6344c3be5982f4362a38208fccf0102bca37f1
pratik dev <pratikdev.iihglobal@gmail.com>
2025-08-27 12:28:15 +0530

    Admin => hide the email tag from post-content

--- resources/views/content/pages/admin/post-content/create.blade.php
+++ resources/views/content/pages/admin/post-content/create.blade.php
@@ -170,7 +170,7 @@
                 ['link', 'image', 'video', 'formula'],
                 [
                     {'insert-name': 'Name'},
-                    {'insert-email': 'Email'},
+                    // {'insert-email': 'Email'},
                     {'insert-phone': 'Phone'},
                     {'insert-website': 'Website'},
                 ],
--- resources/views/content/pages/admin/post-content/edit.blade.php
+++ resources/views/content/pages/admin/post-content/edit.blade.php
@@ -181,7 +181,7 @@
                 ['link', 'image', 'video', 'formula'],
                 [
                     {'insert-name': 'Name'},
-                    {'insert-email': 'Email'},
+                    // {'insert-email': 'Email'},
                     {'insert-phone': 'Phone'},
                     {'insert-website': 'Website'},
                 ],
