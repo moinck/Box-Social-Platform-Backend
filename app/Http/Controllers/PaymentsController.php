2121f267b5c62228bbd914a3e851aafb08c3965e
pratik dev <pratikdev.iihglobal@gmail.com>
2025-08-29 13:08:53 +0530

    Admin => payment history table changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -27,18 +27,24 @@ class PaymentsController extends Controller
                 $name = $payment->user->first_name . ' ' . $payment->user->last_name;
                 return $name;
             })
-            // ->addColumn('payment_id', function ($payment) {
-            //     if ($payment->stripe_payment_intent_id) {
-            //         $subscription = UserSubscription::select('id','stripe_subscription_id')->where('stripe_payment_method_id', $payment->stripe_payment_intent_id)->first();
-            //         return $subscription ? $subscription->stripe_subscription_id : 'N/A';
-            //     }
-            //     return 'N/A';
-            // })
+            ->addColumn('subscription_id', function ($payment) {
+                return $payment->subscription_id ?? "N/A";
+            })
+            ->addColumn('payment_id', function ($payment) {
+                return $payment->stripe_payment_intent_id ?? "N/A";
+            })
             ->addColumn('plan', function ($payment) {
                 return $payment->plan_name;
             })
             ->addColumn('amount', function ($payment) {
                 return $payment->amount . ' ' . strtoupper($payment->currency);
+            })
+            ->addColumn('coupon_discounted_amt', function ($payment) {
+                $discountAmount = $payment->coupon_discounted_amt ?? null;
+                if ($discountAmount) {
+                    return $discountAmount . ' ' . strtoupper($payment->currency);
+                }
+                return "N/A";
             })
             ->addColumn('payment_method', function ($payment) {
                 return $payment->payment_method ? strtoupper($payment->payment_method) : 'N/A';
@@ -46,7 +52,7 @@ class PaymentsController extends Controller
             ->addColumn('created_date', function ($payment) {
                 return Helpers::dateFormate($payment->created_at);
             })
-            ->rawColumns(['user', 'plan', 'amount', 'payment_method', 'created_date'])
+            ->rawColumns(['user', 'plan', 'amount', 'payment_method', 'created_date', 'subscription_id', 'payment_id', 'coupon_discounted_amt'])
             ->make(true);
     }
 }
