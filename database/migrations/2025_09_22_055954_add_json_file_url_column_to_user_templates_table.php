5a4ec623108b0ad6026b58456080c943a692713e
Jay <jayp.iihglobal@gmail.com>
2025-09-22 11:56:25 +0530

    Template JSON file store on DO

---
+++ database/migrations/2025_09_22_055954_add_json_file_url_column_to_user_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_templates', function (Blueprint $table) {
+            $table->string('template_url')->nullable()->after('template_data');
+            $table->tinyInteger('is_uploaded')->nullable()->after('template_url')->comment('1 => Upoaded else not uploaded');
+            $table->timestamp('uploaded_at')->nullable()->after('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_templates', function (Blueprint $table) {
+            $table->dropIfExists('template_url');
+            $table->dropIfExists('is_uploaded');
+            $table->dropIfExists('uploaded_at');
+        });
+    }
+};
