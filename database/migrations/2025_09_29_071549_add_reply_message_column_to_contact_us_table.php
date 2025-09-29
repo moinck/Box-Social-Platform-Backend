18ff223484f187ebe3b8e5e050a78ba425b4313e
Jay <jayp.iihglobal@gmail.com>
2025-09-29 13:23:31 +0530

    Feedback Replay send into mail.

---
+++ database/migrations/2025_09_29_071549_add_reply_message_column_to_contact_us_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            $table->string('email_subject')->after('message')->nullable;
+            $table->text('feedback_reply')->after('email_subject')->nullable;
+            $table->boolean('is_replied')->after('feedback_reply')->nullable;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contact_us', function (Blueprint $table) {
+            $table->dropIfExists('email_subject');
+            $table->dropIfExists('feedback_reply');
+            $table->dropIfExists('is_replied');
+        });
+    }
+};
--- resources/views/content/pages/admin/contact-us/index.blade.php
+++ resources/views/content/pages/admin/contact-us/index.blade.php
@@ -65,6 +65,7 @@
                         <th>Company Name</th>
                         <th>Message</th>
                         <th class="table-date-col">Created Date</th>
+                        <th class="table-action-col">Action</th>
                     </tr>
                 </thead>
             </table>
@@ -72,7 +73,52 @@
     </div>
     <!--/ Main Table -->
 
-    <!--/ Select -->
+    <!-- Start Send Feedback Reply -->
+    <div class="modal fade" id="feedback-reply-modal" tabindex="-1" aria-hidden="true" data-bs-backdrop="static">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-simple">
+            <div class="modal-content">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-body p-0">
+                    <div class="text-center mb-6">
+                        <h4 class="mb-2">Feedback Reply</h4>
+                    </div>
+                    <form id="feedback-reply-form" class="row g-5" method="POST">
+                        @csrf
+                        <div class="col-12">
+                            <div class="form-floating form-floating-outline">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating form-floating-outline">
+                                <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject" required>
+                                <label for="subject">Email Subject</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating form-floating-outline">
+                                <textarea id="message" rows="3" name="message" class="form-control h-px-75"
+                                    placeholder="Enter your message"></textarea>
+                                <label for="message">Message</label>
+                            </div>
+                        </div>
+                        <input type="hidden" id="feedback_id" name="feedback_id" value="">
+                        <div class="col-12 text-center d-flex flex-wrap justify-content-center gap-4 row-gap-4 form-btn">
+                            <button type="submit" class="btn btn-primary">Send</button>
+                            <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
+                                aria-label="Close">
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Send Feedback Reply -->
+
+
 @endsection
 
 <!-- Page Scripts -->
@@ -111,6 +157,7 @@
                         { data: 'company_name', name: 'company_name'},
                         { data: 'message', name: 'message'},
                         { data: 'created_date', name: 'created_date'},
+                        { data: 'action', name: 'action', orderable: false, searchable: false},
                     ],
                     language: {
                         paginate: {
@@ -205,6 +252,156 @@
                 });
             });
             // -------------------------------------------
+
+            //View Feedback Reply Modal
+            $(document).on('click', '.view-reply-btn', function() {
+                var feedBackId = $(this).data('feedback-id');
+                var editUrl = "{{ url('/feedback-management/view-reply/') }}/" + feedBackId;
+                $.ajax({
+                    url: editUrl,
+                    type: "GET",
+                    success: function(response) {
+                        if (response.success == true) {
+                            $(".form-btn").addClass('d-none');
+                            $("#email").val(response.data.email).attr('readonly', true);
+                            $("#subject").val(response.data.email_subject).attr('readonly', true);
+                            $("#message").val(response.data.feedback_reply).attr('readonly', true);
+                            $('#feedback-reply-modal').modal('show');
+                        } else {
+                            // toastr.error(response.message);
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.log(xhr.responseText);
+                    }
+                });
+            });
+            // -------------------------------------------
+
+            //Send Feedback Reply Modal
+            $(document).on('click', '.send-reply-btn', function() {
+                var feedBackId = $(this).data('feedback-id');
+                var table = $('#contact-us-data-table').DataTable();
+
+                // Reset form first
+                $("#feedback-reply-form")[0].reset();
+                $(".form-btn").removeClass('d-none');
+
+                // Make inputs editable
+                $("#subject").attr('readonly', false);
+                $("#message").attr('readonly', false);
+
+                // Get row data
+                var rowData = table.row($(this).parents('tr')).data();
+
+                // Set values
+                $("#feedback_id").val(feedBackId);
+                $("#email").val(rowData.email);
+
+                // Show modal
+                $('#feedback-reply-modal').modal('show');
+            });
+            // -------------------------------------------
+
+            // validate the submit form
+            const sendFeedbackForm = document.getElementById('feedback-reply-form');
+            const sendFeedbackFV = FormValidation.formValidation(sendFeedbackForm, {
+                fields: {
+                    email: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Please enter your email'
+                            },
+                            emailAddress: {
+                                message: 'Please enter a valid email address'
+                            },
+                            stringLength: {
+                                max: 50,
+                                message: 'Email must be less than 50 characters'
+                            }
+                        }
+                    },
+                    subject: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Please enter your email subject'
+                            },
+                            stringLength: {
+                                min: 2,
+                                max: 150,
+                                message: 'Subject must be between 2 and 150 characters'
+                            }
+                        }
+                    },
+                    message: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Please enter your message'
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    bootstrap5: new FormValidation.plugins.Bootstrap5({
+                        eleValidClass: '',
+                        rowSelector: function(field, ele) {
+                            // Customize row selector based on your form layout
+                            if (['email', 'subject', 'message'].includes(field)) {
+                                return '.col-12';
+                            }
+                            return '.col-12';
+                        }
+                    }),
+                    submitButton: new FormValidation.plugins.SubmitButton(),
+                }
+            }).on('core.form.valid', function() {
+                // Form is valid, proceed with form submission
+                var form = $('#feedback-reply-form');
+                var formData = new FormData(form[0]); // Creates FormData object
+                var table = $('#contact-us-data-table').DataTable();
+
+                $.ajax({
+                    url: "{{ route('feedback-management.send-reply') }}",
+                    type: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    beforeSend: function () {
+                        showBSPLoader();
+                    },
+                    complete: function () {
+                        hideBSPLoader();
+                    },
+                    success: function(response) {
+                        if (response.success == true) {
+                            showSweetAlert('success', 'Send !','Feedback reply send successfully.');
+                            $('#feedback-reply-modal').modal('hide');
+                            reloadDataTablePreservingPage(table);
+                        }
+                    },
+                    error: function(xhr) {
+                        hideBSPLoader();
+                        if (xhr.status === 422) {
+                            let errors = xhr.responseJSON.errors;
+                            let firstError = xhr.responseJSON.message;
+
+                            // Show first error in SweetAlert
+                            showSweetAlert('error', 'Validation Error!', firstError);
+
+                            // Or loop through all errors and display
+                            $.each(errors, function(key, value) {
+                                console.log(key + ": " + value[0]);
+                                // You could also place them near the form field if you want
+                            });
+                        } else {
+                            showSweetAlert('error', 'Error!', 'Something went wrong.');
+                        }
+                    }
+                });
+            });
+            // ----------------------------------------------------------
+
         });
     </script>
 @endsection
